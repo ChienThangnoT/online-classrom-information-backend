6c6dd3edf4cde35d801ab2b342169b04340c2b24
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-03-02 01:48:36 +0700

    ViewHistoryPayment
    Students can view the transaction history of courses (including cancellation status, pending payment status, and paid status)

---
+++ LMSystem.Repository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using LMSystem.Repository.Data;
+using LMSystem.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSystem.Repository.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<IEnumerable<Order>> GetOrdersByAccountIdAsync(string accountId);
+
+    }
+}
---
+++ LMSystem.Repository/Repositories/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using LMSystem.Repository.Data;
+using LMSystem.Repository.Interfaces;
+using LMSystem.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace LMSystem.Repository.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        // Assuming you have a DbContext or similar for data access
+        private readonly LMOnlineSystemDbContext _context;
+
+        public OrderRepository(LMOnlineSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByAccountIdAsync(string accountId)
+        {
+            return await _context.Orders
+                .Where(order => order.AccountId == accountId)
+                .ToListAsync();
+        }
+    }
+}
