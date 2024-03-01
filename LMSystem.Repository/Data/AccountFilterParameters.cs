257c241f31b12b351aa99f27d2ec63085e6505ae
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-03-02 02:10:36 +0700

    Fix pagination for account list

--- LMSystem.Repository/Data/AccountFilterParameters.cs
+++ LMSystem.Repository/Data/AccountFilterParameters.cs
@@ -12,4 +12,13 @@ namespace LMSystem.Repository.Data
         public int PageSize { get; set; } = 10;
         public string SortBy { get; set; } = "name";
     }
+
+    public class AccountListResult
+    {
+        public IEnumerable<AccountModelGetList> Accounts { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalAccounts { get; set; }
+        public int TotalPages { get; set; }
+    }
 }
