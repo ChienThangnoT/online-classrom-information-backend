a52afa0faca57751970c208cda9894819179f244
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-03-12 11:40:13 +0700

    Fix main

---
+++ LMSystem.Repository/Data/AddQuizModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSystem.Repository.Data
+{
+    public class AddQuizModel
+    {
+        [Required(ErrorMessage = "Title is required!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required!")]
+        public string Description { get; set; }
+    }
+}
---
+++ LMSystem.Repository/Data/UpdateQuizModel.cs
@@ -0,0 +1,26 @@
+﻿using LMSystem.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSystem.Repository.Data
+{
+    public class UpdateQuizModel
+    {
+        public int QuizId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<UpdateQuestion> Questions { get; set; } = new List<UpdateQuestion>();
+
+    }
+
+    public class UpdateQuestion
+    {
+        public string? QuestionTitle { get; set; } 
+        public int? CorrectAnwser { get; set; }
+        public string? Anwser { get; set; }
+    }
+}
