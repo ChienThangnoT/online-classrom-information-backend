b8488bebcb74150f1297557d2e835592babe01b3
NguyenChienThang <chienthang014@gmail.com>
2024-03-14 15:55:10 +0700

    update model

--- LMSystem.Repository/Data/UpdateProfileModel.cs
+++ LMSystem.Repository/Data/UpdateProfileModel.cs
@@ -10,25 +10,25 @@ namespace LMSystem.Repository.Data
     public class UpdateProfileModel
     {
         [Display(Name = "Name")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [Display(Name = "Name")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email!")]
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid Email!")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Display(Name = "Phone Number")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone Number!")]
         [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Phone Number!")]
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
         [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
-        public string Biography { get; set; }
-        public string ProfileImg { get; set; }
-        public string Sex { get; set; }
+        public string? Biography { get; set; }
+        public string? ProfileImg { get; set; }
+        public string? Sex { get; set; }
     }
 }
